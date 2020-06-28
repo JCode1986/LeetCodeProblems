0607e9fcb0bffdca96577ced827900a3a6d8aa37
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-06-27 19:33:02 -0700

    code completed, with read me and tests

--- Easy/EvenNumberOfDigits/NumberWithEvenNumberOfDigits/Classes/EvenNumberOfDigits.cs
+++ Easy/EvenNumberOfDigits/NumberWithEvenNumberOfDigits/Classes/EvenNumberOfDigits.cs
@@ -6,6 +6,22 @@ namespace NumberWithEvenNumberOfDigits.Classes
 {
     public class EvenNumberOfDigits
     {
-
+        /// <summary>
+        /// Returns a count of even number digits in array
+        /// </summary>
+        /// <param name="nums">array</param>
+        /// <returns>int</returns>
+        public static int FindEvenNumberOfDigits(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (Convert.ToString(nums[i]).Length % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
--- README.md
+++ README.md
@@ -4,4 +4,9 @@
 
 ### Data Strucutres and Algorithms that were completed in LeetCode:
 
-### [Easy]()
+<details>
+<summary>Easy</summary>
+
+* [Find Numbers with Even Number of Digits](https://github.com/JCode1986/LeetCodeProblems/tree/master/Easy/EvenNumberOfDigits/NumberWithEvenNumberOfDigits)
+
+</details>
