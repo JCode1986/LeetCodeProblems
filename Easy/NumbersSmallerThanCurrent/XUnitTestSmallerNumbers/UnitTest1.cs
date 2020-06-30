b544666557891ac8469458118215bc0911ee9788
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-06-29 18:28:33 -0700

    changed names for methods on testings

--- Easy/NumbersSmallerThanCurrent/XUnitTestSmallerNumbers/UnitTest1.cs
+++ Easy/NumbersSmallerThanCurrent/XUnitTestSmallerNumbers/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace XUnitTestSmallerNumbers
         [InlineData(new int[] { 8, 1, 2, 2, 3 }, new int[] { 4, 0, 1, 1, 3 })]
         [InlineData(new int[] { 6, 5, 4, 8 }, new int[] { 2, 1, 0, 3 })]
         [InlineData(new int[] { 7, 7, 7, 7 }, new int[] { 0, 0, 0, 0 })]
-        public void ReturnsCorrectNumberOfEvenNumberedDigits(int[] numbers, int[] expected)
+        public void TestOne(int[] numbers, int[] expected)
         {
             int[] actual = MySmallerNumbers.SmallerNumbersThanCurrent(numbers);
             Assert.Equal(expected, actual);
@@ -20,7 +20,7 @@ namespace XUnitTestSmallerNumbers
         [InlineData(new int[] { 8, 1, 2, 2, 3 }, new int[] { 4, 0, 1, 1, 3 })]
         [InlineData(new int[] { 6, 5, 4, 8 }, new int[] { 2, 1, 0, 3 })]
         [InlineData(new int[] { 7, 7, 7, 7 }, new int[] { 0, 0, 0, 0 })]
-        public void LinqSolutionReturnsCorrectNumberOfEvenNumberedDigits(int[] numbers, int[] expected)
+        public void TestTwo(int[] numbers, int[] expected)
         {
             int[] actual = MySmallerNumbers.LinqSolution(numbers);
             Assert.Equal(expected, actual);
--- Easy/RunningSum/XUnitTestRunningSumArray/UnitTest1.cs
+++ Easy/RunningSum/XUnitTestRunningSumArray/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace XUnitTestRunningSumArray
         [InlineData(new int[] { 1, 2, 3, 4 }, new int[] { 1, 3, 6, 10 } )]
         [InlineData(new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 2, 3, 4, 5 })]
         [InlineData(new int[] { 3, 1, 2, 10, 1 }, new int[] { 3, 4, 6, 16, 17 })]
-        public void ReturnsCorrectNumberOfEvenNumberedDigits(int[] numbers, int[] expected)
+        public void TestOne(int[] numbers, int[] expected)
         {
             int[] actual = MyRunningSumArray.RunningSum(numbers);
             Assert.Equal(expected, actual);
@@ -20,7 +20,7 @@ namespace XUnitTestRunningSumArray
         [InlineData(new int[] { 1, 2, 3, 4 }, new int[] { 1, 3, 6, 10 })]
         [InlineData(new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 2, 3, 4, 5 })]
         [InlineData(new int[] { 3, 1, 2, 10, 1 }, new int[] { 3, 4, 6, 16, 17 })]
-        public void MutateArrayReturnsCorrectNumberOfEvenNumberedDigits(int[] numbers, int[] expected)
+        public void TestTwo(int[] numbers, int[] expected)
         {
             int[] actual = MyRunningSumArray.MutateArrayRunningSum(numbers);
             Assert.Equal(expected, actual);
