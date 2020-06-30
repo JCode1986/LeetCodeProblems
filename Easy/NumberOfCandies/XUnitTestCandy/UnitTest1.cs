8bad743228de5b7498dc692ca60e14bff6aa2fd6
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-06-29 17:59:18 -0700

    challenge completed with tests

--- Easy/NumberOfCandies/XUnitTestCandy/UnitTest1.cs
+++ Easy/NumberOfCandies/XUnitTestCandy/UnitTest1.cs
@@ -1,3 +1,4 @@
+using KidsWithGreatestNumberOfCandies.Classes;
 using System;
 using Xunit;
 
@@ -5,10 +6,15 @@ namespace XUnitTestCandy
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData(new int[] { 2, 3, 5, 1, 3 }, 3, new bool[] { true, true, true, false, true })]
+        [InlineData(new int[] { 4, 2, 1, 1, 2 }, 1, new bool[] { true, false, false, false, false })]
+        [InlineData(new int[] { 12, 1, 12 }, 10, new bool[] { true, false, true })]
 
+        public void ReturnsCorrectNumberOfEvenNumberedDigits(int[] numbers, int value, bool[] expected)
+        {
+            bool[] actual = MyKidsWithCandies.KidsWithCandies(numbers, value);
+            Assert.Equal(expected, actual);
         }
     }
 }
