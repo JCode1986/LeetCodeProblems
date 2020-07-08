4f52159d429ef2ce37c93ee46f1882a6ed25d02c
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-07-07 21:07:21 -0700

    added testing for iterative solution

--- Easy/UnivaluedBinaryTree/XUnitTestUnivaluedTree/UnitTest1.cs
+++ Easy/UnivaluedBinaryTree/XUnitTestUnivaluedTree/UnitTest1.cs
@@ -10,13 +10,27 @@ namespace XUnitTestUnivaluedTree
         [InlineData(new int[] { 1, 1, 1 }, true)]
         [InlineData(new int[] { 1, 2, 1 }, false)]
         [InlineData(new int[] { 3, 2, 1 }, false)]
-        public void Test(int[] values, bool expected)
+        public void Test1(int[] values, bool expected)
         {
             var left = new TreeNode(values[1]);
             var right = new TreeNode(values[2]);
             var root = new TreeNode(values[0], left, right);
 
             bool actual = MyUnivaluedBinaryTree.IsUnivalTreeRecursive(root);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 1, 1 }, true)]
+        [InlineData(new int[] { 1, 2, 1 }, false)]
+        [InlineData(new int[] { 3, 2, 1 }, false)]
+        public void Test2(int[] values, bool expected)
+        {
+            var left = new TreeNode(values[1]);
+            var right = new TreeNode(values[2]);
+            var root = new TreeNode(values[0], left, right);
+
+            bool actual = MyUnivaluedBinaryTree.IsUnivalTreeIterative(root);
             Assert.Equal(expected, actual);
         }
     }
