891bf9eb4fe55db5c24ef38abd6736d8a0f9493f
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-07-07 21:05:41 -0700

    test added

--- Easy/UnivaluedBinaryTree/XUnitTestUnivaluedTree/UnitTest1.cs
+++ Easy/UnivaluedBinaryTree/XUnitTestUnivaluedTree/UnitTest1.cs
@@ -12,12 +12,11 @@ namespace XUnitTestUnivaluedTree
         [InlineData(new int[] { 3, 2, 1 }, false)]
         public void Test(int[] values, bool expected)
         {
-            TreeNode tree = new TreeNode();
-            tree.val = values[0];
-            tree.left.val = values[1];
-            tree.right.val = values[2];
+            var left = new TreeNode(values[1]);
+            var right = new TreeNode(values[2]);
+            var root = new TreeNode(values[0], left, right);
 
-            bool actual = MyUnivaluedBinaryTree.IsUnivalTreeRecursive(tree);
+            bool actual = MyUnivaluedBinaryTree.IsUnivalTreeRecursive(root);
             Assert.Equal(expected, actual);
         }
     }
