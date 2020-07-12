a73ac6206d56ff22b81a91f357906d4b203fcb1c
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-07-11 23:05:41 -0700

    challenge complete with tests

--- Easy/SingleNumber/SingleNumber/Classes/SingleNumber.cs
+++ Easy/SingleNumber/SingleNumber/Classes/SingleNumber.cs
@@ -4,7 +4,36 @@ using System.Text;
 
 namespace SingleNumber.Classes
 {
-    class SingleNumber
+    public class MySingleNumber
     {
+        /// <summary>
+        /// Returns the element that does not appear twice
+        /// </summary>
+        /// <param name="nums">int[]</param>
+        /// <returns>int</returns>
+        public static int SingleNumber(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (!dict.ContainsKey(num))
+                {
+                    dict[num] = +1;
+                }
+                else
+                {
+                    dict[num]++;
+                }
+            }
+
+            foreach (var entry in dict)
+            {
+                if (entry.Value == 1)
+                {
+                    return entry.Key;
+                }
+            }
+            return 0;
+        }
     }
 }
--- Easy/SingleNumber/XUnitTestSingleNumber/UnitTest1.cs
+++ Easy/SingleNumber/XUnitTestSingleNumber/UnitTest1.cs
@@ -1,3 +1,4 @@
+using SingleNumber.Classes;
 using System;
 using Xunit;
 
@@ -5,10 +6,14 @@ namespace XUnitTestSingleNumber
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(new int[] { 2, 2, 1 }, 1)]
+        [InlineData(new int[] { 4, 1, 2, 1, 2 }, 4)]
+        [InlineData(new int[] { 3, 3, 4, 4, 5, 5, 420}, 420)]
+        public void TestOne(int[] numbers, int expected)
         {
-
+            int actual = MySingleNumber.SingleNumber(numbers);
+            Assert.Equal(expected, actual);
         }
     }
 }
