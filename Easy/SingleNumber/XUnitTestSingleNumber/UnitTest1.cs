d0dfbec7d92b75668e5b29fdc8e8e5a9ee441edf
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-07-12 22:33:10 -0700

    added approach in read me

--- Easy/SingleNumber/XUnitTestSingleNumber/UnitTest1.cs
+++ Easy/SingleNumber/XUnitTestSingleNumber/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace XUnitTestSingleNumber
         [InlineData(new int[] { 2, 2, 1 }, 1)]
         [InlineData(new int[] { 4, 1, 2, 1, 2 }, 4)]
         [InlineData(new int[] { 3, 3, 4, 4, 5, 5, 420}, 420)]
+        [InlineData(new int[] { 17, 12, 5, -6, 12, 4, 17, -5, 2, -3, 2, 4, 5, 16, -3, -4, 15, 15, -4, -5, -6 }, 16)]
         public void TestOne(int[] numbers, int expected)
         {
             int actual = MySingleNumber.SingleNumber(numbers);
